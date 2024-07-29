b5acb56b9b287819797bb1912767fa74e8cf1e51
Snazrimuh~ <snazrimuh@gmail.com>
2024-07-29 12:20:13 +0700

    fix footer

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -136,14 +136,13 @@
         </div>
     </section><!-- End Hero -->
 
-    <!-- ======= Footer ======= -->
     <footer id="footer">
 
         <div class="footer-top">
             <div class="container">
                 <div class="row">
 
-                    <div class="col-lg-4 col-md-6 footer-contact">
+                    <div class="col-lg-3 col-md-6 footer-contact">
                         <div style="display: flex; align-items: center;" class="mb-2">
                             <img src="{{ asset('assets/img/sukoharjo.png') }}" alt="logo" class="img-fluid"
                                 style="max-height: 45px;" />
@@ -182,7 +181,7 @@
                         <p> Sabtu : Libur</p>
                     </div>
 
-                    <div class="col-lg-2 col-md-6 footer-links">
+                    <div class="col-lg-3 col-md-6 footer-links">
                         <h4>Aplikasi Desa</h4>
                         <ul>
                             <li><i class="bx bx-chevron-right"></i> <a href="#">Perpustakaan Desa</a></li>
@@ -198,14 +197,14 @@
 
             <div class="me-md-auto text-center text-md-start">
                 <div class="copyright">
-                    &copy; Copyright <strong><span>Lumia</span></strong>. All Rights Reserved
+                    &copy; Hak Cipta 2024 Desa Ngombakan | Kabupaten Sukoharjo
                 </div>
                 <div class="credits">
                     <!-- All the links in the footer should remain intact. -->
                     <!-- You can delete the links only if you purchased the pro version. -->
                     <!-- Licensing information: https://bootstrapmade.com/license/ -->
                     <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/lumia-bootstrap-business-template/ -->
-                    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                    Designed by KKN 239 UNS 2024
                 </div>
             </div>
             <div class="social-links text-center text-md-right pt-3 pt-md-0">
